cc4962857ef8ae0add83cc7155b77473ac4683b2
Djibril <djibrilbarry1011@gmail.com>
2023-11-14 02:39:23 +0100

    premier envoie

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{ uri: 'https://soleil-tech.web.app/' }} // Remplacez par l'URL de la page web que vous souhaitez afficher
+    />
   );
 }
 
